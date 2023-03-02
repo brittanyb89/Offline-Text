bce060a4b52f0d7c92ddde4f4cfa7653fe1232ea
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-03-01 19:44:45 -0600

    Add plugins and babel loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -17,10 +14,57 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    plugins: [],
+    plugins: [
+      // webpack plugin that generates HTML file and injects bundles
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        title: "Offline Text",
+      }),
+      // injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src/sw.js",
+        swDest: "sw.js",
+      }),
+      // generates a manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Offline Text",
+        short_name: "Offline Text",
+        description: "An offline text editor",
+        background_color: "#01579b",
+        theme_color: "#ffffff",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
 
     module: {
-      rules: [],
+      // css loaders
+      rules: [
+        {
+          test: /\.css$/i,
+          exclude: /node_modules/,
+          // use babel-loader in order to use ES6
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
